c5fa5f464a2f784e786dcf2dba4e8d194b0dd3dd
dzejkk <kontrajakub2@gmail.com>
2025-04-04 14:24:41 +0200

    final commit work

--- src/main.js
+++ src/main.js
@@ -3,16 +3,12 @@ import { renderHeader } from "./components/header";
 import { renderFooter } from "./components/footer";
 
 
-
 const initApp = () => {
-
 
     renderHeader();
     renderFooter();
 
-
     initRouter();
 }
-
 
 document.addEventListener('DOMContentLoaded',initApp);
