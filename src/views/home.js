0bc1f2bc3fd370d14302c9c61427d4b05afba7d7
dzejkk <kontrajakub2@gmail.com>
2025-04-04 11:32:14 +0200

    creating SPA setup

---
+++ src/views/home.js
@@ -0,0 +1,13 @@
+export const homeView = () => {
+
+    return `
+    <div class="page home-page">
+      <h1> Vitaj na mojej Vaniila SPA apke</h1>
+      <p>This is the home page of our single page application.</p>
+      <div class="featured-content">
+        <h2>Featured Content</h2>
+        <p>This is some featured content on the home page.</p>
+      </div>
+    </div>
+    `
+};
